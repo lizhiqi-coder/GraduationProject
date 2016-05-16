e404ce57cd9004eb922348959a3a10cb332d7e5b
lizhiqi_lenovo <chinalizhiqi@gmail.com>
2016-05-16 21:19:43 +0800

    增加悬浮菜单按钮

--- app/src/main/java/com/example/admin/graduationproject/activitys/SkyboxActivity.java
+++ app/src/main/java/com/example/admin/graduationproject/activitys/SkyboxActivity.java
@@ -3,10 +3,14 @@ package com.example.admin.graduationproject.activitys;
 import android.opengl.GLSurfaceView;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.view.View;
 import android.widget.Toast;
 
+import com.example.admin.graduationproject.R;
 import com.example.admin.graduationproject.render.SkyboxRender;
 import com.example.admin.graduationproject.utils.AppUtils;
+import com.example.admin.graduationproject.widget.MyDialog;
 
 /**
  * Created by admin on 2016/5/15.
@@ -17,11 +21,16 @@ public class SkyboxActivity extends BaseActivity {
     private SkyboxRender mRender;
     private boolean hasSetRender = false;
 
+    private FloatingActionButton fab;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_skybox);
 
-        glSurfaceView = new GLSurfaceView(this);
+        glSurfaceView = (GLSurfaceView) findViewById(R.id.skybox_surface);
+        fab = (FloatingActionButton) findViewById(R.id.skybox_fab);
+
         mRender = new SkyboxRender(this);
 
         if (AppUtils.isOpenGl2Support()) {
@@ -36,9 +45,29 @@ public class SkyboxActivity extends BaseActivity {
             return;
         }
 
-        setContentView(glSurfaceView);
 
         initTouchListener(glSurfaceView, mRender);
+
+        final MyDialog myDialog = new MyDialog(this, "title", "tip");
+        myDialog.positive.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                myDialog.dismiss();
+            }
+        });
+        myDialog.negative.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                myDialog.dismiss();
+            }
+        });
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                myDialog.show();
+            }
+        });
 
     }
 
